aa097173a0cbac6620f757bc4ce041631e349cf2
Katrina Pritchard <pritckat@gmail.com>
2021-01-15 16:06:51 +0000

    Automatically backed up by Learn

--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,6 +2,24 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurants(state = {
+  restuarants: [],
+  reviews: []
+}, action) {
+  switch (action.type) {
 
+    case 'ADD_RESTAURANT':
+      const restaurant = {text: action.text, id: cuidFn() };
+      return {
+          ...state,
+          restaurants: [...state.restaurants, restaurant]
+      }
+    case 'DELETE_RESTAURANT':
+      const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id);
+      return {...state, restaurants}
+
+    default:
+      return state;
+  }
+  
 }
